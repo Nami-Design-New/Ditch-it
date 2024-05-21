d9bb942130cb20b5157054f7b6a120316a292a11
Ahmedelsayed265 <ahmedelsayed2102@icloud.com>
2024-05-21 14:06:51 +0300

    start

--- js/custom.js
+++ js/custom.js
@@ -27,9 +27,7 @@ $(document).ready(function () {
       return Math.max(videoDuration, minimumDelay);
     }
     return minimumDelay;
-    x;
   }
-  // main Slider
   const mainSlider = new Swiper(".mainSlider", {
     spaceBetween: 0,
     loop: true,
@@ -41,12 +39,12 @@ $(document).ready(function () {
       disableOnInteraction: false,
     },
     pagination: {
-      el: ".mainSliderPagination",
+      el: ".mainSlider .swiper-pagination",
       clickable: true,
     },
     navigation: {
-      nextEl: ".mainSliderNext",
-      prevEl: ".mainSliderPrev",
+      nextEl: ".mainSlider .swiper-button-next",
+      prevEl: ".mainSlider .swiper-button-prev",
     },
     keyboard: {
       enabled: true,
@@ -67,21 +65,37 @@ $(document).ready(function () {
       },
     },
   });
-  //category Slider
-  var categorySlider = new Swiper(".categorySlider", {
+  function setAutoplayDelay(slider) {
+    const activeSlide = slider.slides[slider.activeIndex];
+    const activeVideo = activeSlide.querySelector(".mainSlider video");
+    const autoplayDelay = calculateAutoplayDelay(activeVideo, 8000);
+    slider.params.autoplay.delay = autoplayDelay;
+    slider.autoplay.start();
+    console.log("Swiper Autoplay Delay:", autoplayDelay);
+  }
+  function pauseAllVideos() {
+    const allVideos = document.querySelectorAll(".mainSlider video");
+    allVideos.forEach(function (video) {
+      video.pause();
+    });
+  }
+
+  //populr Slider
+  var popular = new Swiper(".popular .swiper", {
     pagination: {
-      el: ".categorySliderPagination",
+      el: ".popular .swiper-pagination",
       clickable: true,
     },
     navigation: {
-      nextEl: ".categorySliderNext",
-      prevEl: ".categorySliderPrev",
+      nextEl: ".popular .swiper-button-next",
+      prevEl: ".popular .swiper-button-prev",
     },
     // centeredSlides: true,
     // loop: true,
     slidesPerView: "auto",
+    watchSlidesProgress: true,
     spaceBetween: 10,
-    speed: 1000,
+    speed: 700,
     autoplay: {
       delay: 2500,
       disableOnInteraction: false,
@@ -100,103 +114,31 @@ $(document).ready(function () {
         slidesPerView: 5,
       },
       1200: {
-        slidesPerView: 6,
+        slidesPerView: 4,
       },
     },
   });
-  function setAutoplayDelay(slider) {
-    const activeSlide = slider.slides[slider.activeIndex];
-    const activeVideo = activeSlide.querySelector(".mainSlider video");
-    const autoplayDelay = calculateAutoplayDelay(activeVideo, 8000);
-    slider.params.autoplay.delay = autoplayDelay;
-    slider.autoplay.start();
-    console.log("Swiper Autoplay Delay:", autoplayDelay);
-  }
-  // Function to pause all videos
-  function pauseAllVideos() {
-    const allVideos = document.querySelectorAll(".mainSlider video");
-    allVideos.forEach(function (video) {
-      video.pause();
-    });
-  }
+
   // text length
-  $(".project .projectInfo .description").each(function () {
+  $(".item .itemInfo .title , .item .itemInfo .location span").each(function () {
     var text = $(this).text();
-    if (text.length > 90) {
+    if (text.length > 35) {
       var truncatedText =
-        $.trim(text).substring(0, 90).split(" ").slice(0, -1).join(" ") + "...";
+        $.trim(text).substring(0, 35).split(" ").slice(0, -1).join(" ") + "...";
       $(this).text(truncatedText);
     }
   });
-  // quantity
-  $(".plus").click(function () {
-    $(this)
-      .prev()
-      .val(+$(this).prev().val() + 1);
-  });
-  $(".min").click(function () {
-    if ($(this).next().val() > 0)
-      $(this)
-        .next()
-        .val(+$(this).next().val() - 1);
-  });
-  $(".copyText").click(function () {
-    var text = $(this).prev("p").text();
-    navigator.clipboard.writeText(text).then(function () {
-      $("#alertMessage").slideDown().delay(1500).fadeOut();
-    });
-  });
-  $(".project , .projectDetailsPage").each(function () {
-    var $project = $(this);
-    var $gifts = $project.find(".gifts");
-    var $inputDiv = $project.find(".inputDiv");
-    var $quantity = $project.find(".quantity");
-    $inputDiv.hide();
-    $quantity.hide();
-    $gifts.find(".gift, .donate").click(function () {
-      $gifts.find(".gift, .donate").removeClass("active");
-      $(this).addClass("active");
-      $project.find(".action span").remove();
-      if ($(this).hasClass("gift")) {
-        $quantity.show();
-        $inputDiv.hide();
-      } else if ($(this).hasClass("donate")) {
-        $quantity.hide();
-        $inputDiv.show().find("input").focus();
-      }
-    });
+  // text length
+  $(".item .itemInfo .description").each(function () {
+    var text = $(this).text();
+    if (text.length > 110) {
+      var truncatedText =
+        $.trim(text).substring(0, 110).split(" ").slice(0, -1).join(" ") +
+        "...";
+      $(this).text(truncatedText);
+    }
   });
 });
-
-// theme costomization
-$(document).ready(function () {
-  // Retrieve values ​​from local storage if available
-  var mainColor = localStorage.getItem("mainColor");
-  var mainColor20 = localStorage.getItem("mainColor20");
-  var mainColor10 = localStorage.getItem("mainColor10");
-
-  if (mainColor) {
-    $(":root").css("--mainColor", mainColor);
-    $("#colorInput").val(mainColor);
-  }
-  // Update values ​​if available in local storage
-  if (mainColor) $(":root").css("--mainColor", mainColor);
-  if (mainColor20) $(":root").css("--mainColor20", mainColor20);
-  if (mainColor10) $(":root").css("--mainColor10", mainColor10);
-
-  $("#colorInput").on("input", function () {
-    var newColor = $(this).val();
-    $(":root").css("--mainColor", newColor);
-    $(":root").css("--mainColor20", newColor + "33"); // Adds 20% opacity
-    $(":root").css("--mainColor10", newColor + "1a"); // Adds 10% opacity
-
-    // Save the values ​​to local storage
-    localStorage.setItem("mainColor", newColor);
-    localStorage.setItem("mainColor20", newColor + "33");
-    localStorage.setItem("mainColor10", newColor + "1a");
-  });
-});
-
 // ////////////////////////////////////////
 // ////////////////////////////////////////
 // ////////////////////////////////////////
